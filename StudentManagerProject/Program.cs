41192578cdf827ebaaddc37410295603d2ccf03d
vutnq <tnqv1021997@gmail.com>
2017-07-04 23:18:35 +0700

    Update execute query , and execute non query

--- StudentManagerProject/Program.cs
+++ StudentManagerProject/Program.cs
@@ -18,6 +18,7 @@ namespace StudentManagerProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
