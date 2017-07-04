dc0c98cf289ec814217d73c51579a238b24c9ee0
canhvedepzai <tphan2883@gmail.com>
2017-07-04 19:56:02 +0700

    Tri test

--- StudentManagerProject/Program.cs
+++ StudentManagerProject/Program.cs
@@ -17,6 +17,7 @@ namespace StudentManagerProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("WTF");
         }
     }
 }
