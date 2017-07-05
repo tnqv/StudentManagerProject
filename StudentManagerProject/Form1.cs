3ba09b280058b7f1c8b64c766398cc1c2c75f7d1
vutnq <tnqv1021997@gmail.com>
2017-07-05 08:07:00 +0700

    Update UI for management

--- StudentManagerProject/Form1.cs
+++ StudentManagerProject/Form1.cs
@@ -7,17 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace StudentManagerProject
 {
     public partial class Form1 : Form
     {
+        public SqlDataAdapter adapter;
+        public DataTable data;
         public Form1()
         {
             InitializeComponent();
+            initialDataTable();
+        }
+
+        private void initialDataTable()
+        {
+            SqlConnection con = DBUtils.GetDBConnection();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tbl_students");
+            data = DBUtils.ExecuteQuery(cmd,con);  
+        }
+
+        private void loadData()
+        {
+            try
+            {
+                dataGridView1.DataSource = data;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Cannot load data " + e.Message);
+                dataGridView1.DataSource = null;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
         {
 
         }
